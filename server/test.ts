b10363fc75e9994930aca65a14c2d7cce48e433d
pampams211 <pampams211@gmail.com>
2025-04-19 07:55:17 +0000

    inisiasi

---
+++ server/test.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import {v2 as cloudinary} from 'cloudinary'
+import {z} from 'zod'
+import { safeClient } from './save-action'
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
+const formSchema = z.object({
+    publicId: z.string()
+})
+
+export const test = safeClient
+.schema(formSchema)
+.action(async ({parsedInput:{publicId}}) => {
+    console.log('test')
+
+    try {
+        const result = await cloudinary.api.resource(`${publicId}.transcript`, {
+            'resource_type':'raw'
+        })
+        console.log(result)
+        return {success: result}
+    } catch (error) {
+        console.log(error)
+    }
+})
